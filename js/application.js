2bc51818833306dec163f59eaeb5fd5aa3bff801
GumZ <davidgumzchoi@gmail.com>
2014-06-04 23:32:44 -0700

    added js comments

---
+++ js/application.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('article h1');
+    $('article h2');
+
+    var h1Basecamp = 'Basecamp is the project management tool you wish you had on your last project.';
+    var h2Basecamp = 'Are you still managing projects with email?  Are you still using Excel for your to-do lists?  Its time to upgrade to Basecamp.  Manage projects and collaborate with your team and clients the modern way.';
+    var h1Highrise = "Highrise remembers the important things about people you'd normally forget.";
+    var h2Highrise = 'Keep a permenant record of people you do business with.  Know who you talked to, when you talked to them, what was said, and when to follow up next.  Over 20,000,000 contacts are tracked using Highrise.';
+    var h1Campfire = 'From near or far, Campfire helps teams work together over the web in real-time.';
+    var h2Campfire = "Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room.  We couldn't run our own business without Campfire.";
+
+    // hide left arrow
+    // hide right arrow
+    // left arrow appear on hover
+    // right arrow appear on hover
+    // default on mouse out
+    // replace h1/h2 on hover
+    // default on mouse out
+
+});
